2d431b8414877fbfd9da575ea4c0d316e9548f28
umesha-hb <umesha.kit@gmail.com>
2023-05-20 11:51:23 +0530

    first commit

---
+++ src/main/java/com/uttara/awscodepipeline/docker/ecr/AwsCodePipelineDockerEcrApplication.java
@@ -0,0 +1,13 @@
+package com.uttara.awscodepipeline.docker.ecr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCodePipelineDockerEcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCodePipelineDockerEcrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uttara/awscodepipeline/docker/ecr/AwsCodePipelineDockerEcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uttara.awscodepipeline.docker.ecr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCodePipelineDockerEcrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
